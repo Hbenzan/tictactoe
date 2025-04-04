842f9401f7d6c5490ea0aee38e6563169ddfe4b1
hayesbenzan <hayes_benzan25@milton.edu>
2025-04-04 12:27:54 -0400

    created the project

---
+++ src/CustomComponent.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function CustomComponent() {
+  return (
+    <div >
+        <h1>Hello from custom component!</h1>
+    </div>
+  );
+}
+
+export default CustomComponent;
